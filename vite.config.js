e839d16df975f0f218c03496392660750f8c0864
piussalami2050-spec <piussalami2050@gmail.com>
2026-01-21 18:28:44 +0100

    Remove unnecessary code block formatting from config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-```javascript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: './'
 })
-```
